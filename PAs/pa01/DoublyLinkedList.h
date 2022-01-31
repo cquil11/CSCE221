a52233600fa50cfc4a1dedc60ccb83a6223cf441
cquil11 <cjquilici@gmail.com>
2022-01-30 19:54:05 -0600

    Finished initial version of DoublyLinkedList.h
    No testing has been done yet

--- PAs/pa01/DoublyLinkedList.h
+++ PAs/pa01/DoublyLinkedList.h
@@ -1,8 +1,33 @@
 // TODO: Define the DoublyLinkedList class
 
-// Added this line to test git functionality
-// Hello!
-// Again...
-// Again..
+#ifndef DOUBLY_LINKEDLIST
+#define DOUBLY_LINKEDLIST
 
+#include <iostream>
+#include <string>
+#include "Node.h"
 
+class DoublyLinkedList
+{
+private:
+    Node *head;
+    Node *tail;
+    size_t length;
+
+public:
+    DoublyLinkedList();
+    DoublyLinkedList(const DoublyLinkedList &other);
+    DoublyLinkedList &operator=(const DoublyLinkedList &other);
+    ~DoublyLinkedList();
+
+    size_t getLength() const;
+    Node *getFront() const;
+    Node *getBack() const;
+
+    void insert(NetworkPacket data, int index);
+    void remove(int index);
+    void clear();
+    std::string toString();
+};
+
+#endif
