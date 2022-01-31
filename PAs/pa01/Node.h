c02aa4cd55a0c7a80f308e6a6bdb86efd634390d
cquil11 <cjquilici@gmail.com>
2022-01-30 19:31:51 -0600

    Completed Node.h class
    Still unsure what prof means when he says that 'you will need a default constructor for this one'

--- PAs/pa01/Node.h
+++ PAs/pa01/Node.h
@@ -5,4 +5,23 @@
 // TODO: Define the Node class
 // Note: Since the functions are all small, you can put everything in this file
 
+class Node
+{
+public:
+    NetworkPacket data;
+    Node *next;
+    Node *prev;
+
+    Node() : data(NetworkPacket()), next(nullptr), prev(nullptr) {}
+    Node(const NetworkPacket &d) : data(d), next(nullptr), prev(nullptr) {}
+
+    NetworkPacket getData() { return data; }
+
+    Node *getNext(){return next; }
+    Node *getPrev() { return prev; }
+    void setData(const NetworkPacket &d) { data = d; }
+    void setNext(Node *n) { next = n; }
+    void setPrev(Node *p) { prev = p; }
+};
+
 #endif
